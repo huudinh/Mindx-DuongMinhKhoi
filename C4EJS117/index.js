a89baa063275bbf44e2622d58b191727e12e8df6
huudinh <dinhndvn@yahoo.com>
2021-05-29 20:47:55 +0700

    index.html

---
+++ C4EJS117/index.js
@@ -0,0 +1,23 @@
+{
+    //input
+    // let x = prompt('Nhap so x:');
+    // let s = prompt('Nhap cau chao s:');
+    // let x = 5 * 5;
+    // let s = 'hello';
+    // x = x.toUpperCase();
+    // x = x + 5;
+    // x -= 5;
+    // x--;
+
+    //process update
+    // x = 6;
+    // s = 'hi there';
+
+    let s = 198;
+
+    s += 5;
+
+    //out put
+    console.log(s);
+    // console.log(s);
+}
---
+++ Khoi/index.js
@@ -0,0 +1,15 @@
+//input
+let x = 2 * 3 + 5;
+// let s = prompt('Nhap nhap loi chao: ');
+// let x = 5;
+// let s = 'Hellow world';
+
+// process
+// x = 6;
+// s = 'hi there';
+// s = x + s;
+
+
+// output
+console.log(`Chao ban Khoi, ban sinh nam ${x}`);
+// console.log(s);
